709dda56c09d4bc428fd30ef30602749f129df58
vranabothu <vranabothu@digitalocean.com>
2026-04-02 10:26:28 +0530

    Update ContextWindow string and Bump version

--- cmd/mcp-digitalocean/main.go
+++ cmd/mcp-digitalocean/main.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	mcpName                 = "mcp-digitalocean"
-	mcpVersion              = "1.0.41"
+	mcpVersion              = "1.0.42"
 	wsLoggingContextTimeout = 15 * time.Second
 )
 
